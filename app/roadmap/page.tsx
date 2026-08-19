66bce97b9bd3130f0cad8e10369e446339ce23f6
Akari1997 <yiqing.wang@lego.com>
2026-08-19 15:49:51 +0800

    Revert "feat(roadmap): add roadmap page route"
    This reverts commit 9994a69d831c431a9630b85d3d8f26415deb7071.

--- app/roadmap/page.tsx
+++
@@ -1,9 +0,0 @@
-import RoadmapCanvas from "@/components/roadmap/RoadmapCanvas";
-import { loadRoadmap, toFlow } from "@/lib/roadmap/transform";
-
-export default function RoadmapPage() {
-  const data = loadRoadmap();
-  const { nodes, edges } = toFlow(data);
-
-  return <RoadmapCanvas initialNodes={nodes} initialEdges={edges} title={data.title} />;
-}
